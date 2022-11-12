6ca36597396641a91f202a35c5fbd58feb0c60ce
Danylo-Voloshyn <danylovoloshyn0@gmail.com>
2022-11-11 23:03:46 -0500

    sierpinski

--- main.py
+++ main.py
@@ -14,36 +14,37 @@ window = pygame.display.set_mode((w, h))
 
 black = (0, 0, 0)
 white = (255, 255, 255)
-pts = 1000000
 
-sierp = ((100, h - 100), (w - 100, h - 100), (int(w / 2), 100))
+tri = ((100, h - 100), (w - 100, h - 100), (int(w / 2), 100))
+
+pts = 4 * pow(10, 6)
 
 
-def draw_sierpinski():
-    pt = sierp[2]
+def draw_triangle():
+    frac = 2
+    pt = tri[2]
     window.set_at(pt, white)
 
-    for i in range(pts):
-        if i % 100 == 0:
+    for n in range(pts):
+        if n % 100 == 0:
             display.update()
-        vtx = int(random() * 3)
-        pt = (int(pt[0] + (sierp[vtx][0] - pt[0]) / 2), int(pt[1] + (sierp[vtx][1] - pt[1]) / 2))
+        i = int(random() * 3)
+        pt = (int(pt[0] + (tri[i][0] - pt[0]) / frac), int(pt[1] + (tri[i][1] - pt[1]) / frac))
         window.set_at(pt, white)
-        # print(i) # for debugging
     print('DONE')
 
 
 def draw_creepers():
     """
-a failed attempt produced something terrifying
+a failed attempt produced something cryptic
     """
-    pt = sierp[2]
+    pt = tri[2]
     window.set_at(pt, white)
 
-    for i in range(pts):
-        vtx = int(random() * 3)
-        pt = (int(abs(pt[0] - sierp[vtx][0]) / 2), int(abs(pt[1] - sierp[vtx][1]) / 2))
-        window.set_at(pt, white)
+    for n in range(pts):
+        i = int(random() * 3)
+        pt = (abs(pt[0] - tri[i][0]) / 1.75, abs(pt[1] - tri[i][1]) / 1.75)
+        window.set_at((int(pt[0] * 1.85), int(pt[1] * 1.85)), white)  # zooming in
     print('DONE')
 
 
@@ -53,12 +54,12 @@ def draw_bounds(vertices):
 
 
 def create_window():
-    pygame.display.set_caption("Chaos Game")
+    pygame.display.set_caption('Chaos Game')
     window.fill(black)
     pygame.display.flip()
 
-    # draw_bounds(sierp)
-    draw_sierpinski()
+    # draw_bounds(tri)
+    draw_triangle()
     # draw_creepers()
 
     display.update()
