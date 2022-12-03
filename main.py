3ce4babb58674ce9908146f3ecbc21de854c9646
Danylo <danylovoloshyn0@gmail.com>
2022-12-02 19:10:03 -0500

    start carpet

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import time
 from math import cos, pi, sin
 from random import random
@@ -6,26 +7,39 @@ import pygame
 from pygame import display
 
 # the size of circle on which the vertices of the polygon are placed
-size = 1000
+size = 900
+bd = 25  # border
 
-w = size + 25
-h = size + 25
+w = size + bd
+h = size + bd
 window = pygame.display.set_mode((w, h))
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-pts = 4 * pow(10, 6)
-updates = 50
+pts = 1 * pow(10, 5)
+updates = 10
 
 
-def draw_reg_poly_fractal(n, frac):
+def draw_carpet():
+    coords = ((bd, bd), (bd + size, bd), (bd, bd + size), (bd + size, bd + size),
+              )
+
+
+def draw_perfect_polyflake(n):
+    # approximate function for distance to jump
+    # each section of the fractal (with n sections) is close together
+    draw_polyflake(n, 3 / n + 1)
+
+
+def draw_polyflake(n, frac):
     vertices = reg_poly_vertices(n)
     pt = vertices[0]
     window.set_at(pt, white)
 
     for i in range(1, pts + 1):
         if i % (pts / updates) == 0:
+            time.sleep(0.25)
             display.update()
         j = int(random() * n)
         pt = (int(pt[0] + (vertices[j][0] - pt[0]) / frac), int(pt[1] + (vertices[j][1] - pt[1]) / frac))
@@ -41,7 +55,8 @@ def draw_reg_poly_vertices(n):
 def reg_poly_vertices(n):
     vertices = []
     for i in range(n):
-        vertices.append((int(w / 2 + (size / 2) * cos(2 * pi * i / n)), int(h / 2 + (size / 2) * sin(2 * pi * i / n))))
+        vertices.append((int(w / 2 + (size / 2) * cos(2 * pi * i / n - pi / 2)),
+                         int(h / 2 + (size / 2) * sin(2 * pi * i / n - pi / 2))))
     return vertices
 
 
@@ -51,12 +66,16 @@ def create_window():
     pygame.display.flip()
 
     initial = float(time.time() * 1000)
-    draw_reg_poly_fractal(9, 1.6)
+
+    # draw_perfect_polyflake(3)
+    draw_carpet()
+    
     final = float(time.time() * 1000)
     print(str(round((final - initial) / 1000, 4)) + 's')
 
     while True:
         for event in pygame.event.get():
+	        display.update()
             if event.type == pygame.MOUSEBUTTONUP:
                 pygame.quit()
             if event.type is pygame.QUIT:
