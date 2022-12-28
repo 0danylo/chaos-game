0aad733117d76b612975aec295310f0ee4c84a86
Danylo <danylovoloshyn0@gmail.com>
2022-12-28 01:41:42 -0500

    fractal restricted by vertex options

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import time
 from math import cos, pi, sin
-from random import random
 
 import pygame
-from pygame import display
 
-from unconstrained_fractal import fractal_avoiding_center_circle
+from fractal_in_circle import *
 
 size = 950  # size of the fractal
 bd = 25  # border
@@ -19,44 +16,45 @@ pt_clr = (br,) * 3
 clrs = ((br, 0, 0), (0, br, 0), (0, 0, br),
         (br, br, 0), (br, 0, br), (0, br, br))
 
-iterations = 5 * pow(10, 6)
+iterations = 2 * pow(10, 6)
 updates = 10
 
 
 def main():
-	pygame.display.set_caption('Chaos Game')
-	window.fill(bg_clr)
-	pygame.display.flip()
-	
-	initial = float(time.time() * 1000)
-	
-	fractal_avoiding_center_circle(100, 2.5)  # !!!
-	
-	final = float(time.time() * 1000)
-	print(str(round((final - initial) / 1000, 4)) + 's')
-	
-	while True:
-		for event in pygame.event.get():
-			display.update()
-		if event.type == pygame.MOUSEBUTTONUP:
-			pygame.quit()
-		if event.type is pygame.QUIT:
-			break
+    initial = float(time.time() * 1000)
+
+    from regular_fractal import restricted_fractal_1
+    restricted_fractal_1(2)
+    
+    final = float(time.time() * 1000)
+    print(str(round((final - initial) / 1000, 4)) + 's')
+    
+    while True:
+        for event in pygame.event.get():
+            display.update()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pygame.quit()
+        if event.type is pygame.QUIT:
+            break
 
 
 def get_reg_poly_vertices(n):
-	vertices = []
-	for i in range(n):
-		vertices.append((int(w / 2 + (size / 2) * cos(2 * pi * i / n - pi / 2)),
-		                 int(h / 2 + (size / 2) * sin(2 * pi * i / n - pi / 2))))
-	return vertices
+    vertices = []
+    for i in range(n):
+        vertices.append((int(w / 2 + (size / 2) * cos(2 * pi * i / n - pi / 2)),
+                         int(h / 2 + (size / 2) * sin(2 * pi * i / n - pi / 2))))
+    return vertices
 
 
 def reg_poly_vertices(n):
-	for v in get_reg_poly_vertices(n):
-		window.set_at((int(v[0]), int(v[1])), pt_clr)
+    for v in get_reg_poly_vertices(n):
+        window.set_at((int(v[0]), int(v[1])), pt_clr)
 
 
 if __name__ == '__main__':
-	pygame.init()
-	main()
+    pygame.init()
+    pygame.display.set_caption('Chaos Game')
+    window.fill(bg_clr)
+    pygame.display.flip()
+    
+    main()
