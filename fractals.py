f08a1d922c7b6df35e0279472afa78a2bb5dae0d
Danylo <danylovoloshyn0@gmail.com>
2023-01-01 22:25:48 -0800

    more cool fractals and experimentation

--- fractals.py
+++ fractals.py
@@ -6,14 +6,14 @@ from main import *
 def basic_fractal(verts, zoom):
     pt = verts[0]
     
-    for i in range(1, iterations + 1):
-        if i % (iterations / updates) == 0:
+    for i in range(1, points + 1):
+        if i % (points / updates) == 0:
             time.sleep(0.25)
             display.update()
         
         j = int(random() * len(verts))
-        pt = (int(pt[0] + (verts[j][0] - pt[0]) / zoom),
-              int(pt[1] + (verts[j][1] - pt[1]) / zoom))
+        pt = (pt[0] + (verts[j][0] - pt[0]) / zoom,
+              pt[1] + (verts[j][1] - pt[1]) / zoom)
         draw_at(pt)
 
 
@@ -43,15 +43,15 @@ def fractal_avoiding_center_circle(r, zoom):
     verts = ((bd, bd), (bd + size, bd), (bd, bd + size), (bd + size, bd + size))
     pt_q = [verts[0], verts[0]]
     
-    for i in range(1, iterations + 1):
+    for i in range(1, points + 1):
         # if i % (pts / updates) == 0:
         #     time.sleep(0.25)
         #     display.update()
         
         j = int(random() * 4)
         pt_q[0] = pt_q[1]
-        pt_q[1] = (int(pt_q[1][0] + (verts[j][0] - pt_q[1][0]) / zoom),
-                   int(pt_q[1][1] + (verts[j][1] - pt_q[1][1]) / zoom))
+        pt_q[1] = (pt_q[1][0] + (verts[j][0] - pt_q[1][0]) / zoom,
+                   pt_q[1][1] + (verts[j][1] - pt_q[1][1]) / zoom)
         if (pt_q[1][0] - w / 2) ** 2 + (pt_q[1][1] - h / 2) ** 2 < r ** 2:
             i -= 1
             pt_q[1] = pt_q[0]
@@ -67,11 +67,11 @@ def fractal_avoiding_center_circle(r, zoom):
 def ic_basic(verts, zoom, r):
     pt_q = [verts[0], verts[0]]
     
-    for i in range(1, iterations + 1):
+    for i in range(1, points + 1):
         j = int(random() * len(verts))
         pt_q[0] = pt_q[1]
-        pt_q[1] = (int(pt_q[1][0] + (verts[j][0] - pt_q[1][0]) / zoom),
-                   int(pt_q[1][1] + (verts[j][1] - pt_q[1][1]) / zoom))
+        pt_q[1] = (pt_q[1][0] + (verts[j][0] - pt_q[1][0]) / zoom,
+                   pt_q[1][1] + (verts[j][1] - pt_q[1][1]) / zoom)
         if (pt_q[1][0] - w / 2) ** 2 + (pt_q[1][1] - h / 2) ** 2 > r ** 2:
             i -= 1
             pt_q[1] = pt_q[0]
@@ -105,11 +105,11 @@ def ic_avoid_center(r_i, zoom, r_o):
     verts = ((bd, bd), (bd + size, bd), (bd, bd + size), (bd + size, bd + size))
     pt_q = [verts[0], verts[0]]
     
-    for i in range(1, iterations + 1):
+    for i in range(1, points + 1):
         j = int(random() * 4)
         pt_q[0] = pt_q[1]
-        pt_q[1] = (int(pt_q[1][0] + (verts[j][0] - pt_q[1][0]) / zoom),
-                   int(pt_q[1][1] + (verts[j][1] - pt_q[1][1]) / zoom))
+        pt_q[1] = (pt_q[1][0] + (verts[j][0] - pt_q[1][0]) / zoom,
+                   pt_q[1][1] + (verts[j][1] - pt_q[1][1]) / zoom)
         
         if (pt_q[1][0] - w / 2) ** 2 + (pt_q[1][1] - h / 2) ** 2 < r_i ** 2 or \
            (pt_q[1][0] - w / 2) ** 2 + (pt_q[1][1] - h / 2) ** 2 > r_o ** 2:
@@ -124,7 +124,7 @@ def ic_avoid_center(r_i, zoom, r_o):
 """
 
 
-def restricted_fractal_2(zoom):
+def restricted_fractal_2(n, zoom):
     """
         written by GitHub Copilot
 
@@ -132,21 +132,21 @@ def restricted_fractal_2(zoom):
         and the next vertex cannot be the previous vertex
     """
     # verts = ((bd, bd), (bd + size, bd), (bd, bd + size), (bd + size, bd + size))  # Z-pattern of points
-    verts = ((bd, bd), (bd + size, bd), (bd + size, bd + size), (bd, bd + size))
+    verts = get_reg_poly_vertices(n)
     vt_q = [verts[0], verts[0]]
     pt = verts[0]
     draw_at(pt)
     
-    for i in range(1, iterations + 1):
-        j = int(random() * 4)
+    for i in range(1, points + 1):
+        j = int(random() * n)
         vt_q[0] = vt_q[1]
         vt_q[1] = verts[j]
         
-        if vt_q[0] == vt_q[1] or vt_q[0] == verts[(j + 1) % 4]:
+        if vt_q[0] == vt_q[1] or vt_q[0] == verts[(j + 1) % n]:
             i -= 1
             continue
-        pt = (int(pt[0] + (vt_q[1][0] - pt[0]) / zoom),
-              int(pt[1] + (vt_q[1][1] - pt[1]) / zoom))
+        pt = (pt[0] + (vt_q[1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[1][1] - pt[1]) / zoom)
         draw_at(pt)
 
 
@@ -159,7 +159,7 @@ def no_repeats(n, zoom):
     pt = verts[0]
     draw_at(pt)
     
-    for i in range(1, iterations + 1):
+    for i in range(1, points + 1):
         j = int(random() * n)
         vt_q[0] = vt_q[1]
         vt_q[1] = verts[j]
@@ -167,68 +167,49 @@ def no_repeats(n, zoom):
         if vt_q[0] == vt_q[1]:
             i -= 1
             continue
-        pt = (int(pt[0] + (vt_q[1][0] - pt[0]) / zoom),
-              int(pt[1] + (vt_q[1][1] - pt[1]) / zoom))
+        pt = (pt[0] + (vt_q[1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[1][1] - pt[1]) / zoom)
         draw_at(pt)
 
 
-def no_cw_neighbors(n, zoom):
+def no_neighbors_n_away_cw(sides, n, zoom):
     """
         the current vertex cannot be one place away (clockwise) from the previously chosen vertex
     """
-    verts = get_reg_poly_vertices(n)
+    verts = get_reg_poly_vertices(sides)
     vt_q = [verts[0], verts[0]]
     pt = verts[0]
     draw_at(pt)
     
-    for i in range(1, iterations + 1):
-        j = int(random() * n)
+    for i in range(1, points + 1):
+        j = int(random() * sides)
         
-        if vt_q[1] == verts[(j + 1) % n]:
+        if vt_q[1] == verts[(j + n) % sides]:
             i -= 1
             continue
         
         vt_q[0] = vt_q[1]
         vt_q[1] = verts[j]
-        pt = (int(pt[0] + (vt_q[1][0] - pt[0]) / zoom),
-              int(pt[1] + (vt_q[1][1] - pt[1]) / zoom))
-        draw_at(pt)
-    
-    
-def no_opposite_verts(n, zoom):
-    """
-        the current vertex cannot be chosen in the next iteration
-        and the next vertex cannot be the opposite vertex
-    """
-    verts = get_reg_poly_vertices(n)
-    vt_q = [verts[0], verts[0]]
-    pt = verts[0]
-    draw_at(pt)
-    
-    for i in range(1, iterations + 1):
-        j = int(random() * n)
-        
-        if vt_q[1] == verts[(j + 2) % n]:
-            i -= 1
-            continue
-        vt_q[0] = vt_q[1]
-        vt_q[1] = verts[j]
-        pt = (int(pt[0] + (vt_q[1][0] - pt[0]) / zoom),
-              int(pt[1] + (vt_q[1][1] - pt[1]) / zoom))
+        pt = (pt[0] + (vt_q[1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[1][1] - pt[1]) / zoom)
         draw_at(pt)
         
         
 def no_cw_neighbors_2(n, zoom):
     """
+        written by GitHub Copilot
+        
         the current vertex cannot be one place away (clockwise) from the previously chosen vertex
         and the next vertex cannot be the opposite vertex
+        
+        (actually, the next vertex cannot be two places away (clockwise))
     """
     verts = get_reg_poly_vertices(n)
     vt_q = [verts[0], verts[0]]
     pt = verts[0]
     draw_at(pt)
     
-    for i in range(1, iterations + 1):
+    for i in range(1, points + 1):
         j = int(random() * n)
         
         if vt_q[1] == verts[(j + 1) % n] or vt_q[1] == verts[(j + 2) % n]:
@@ -237,6 +218,79 @@ def no_cw_neighbors_2(n, zoom):
         
         vt_q[0] = vt_q[1]
         vt_q[1] = verts[j]
-        pt = (int(pt[0] + (vt_q[1][0] - pt[0]) / zoom),
-              int(pt[1] + (vt_q[1][1] - pt[1]) / zoom))
+        pt = (pt[0] + (vt_q[1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[1][1] - pt[1]) / zoom)
+        draw_at(pt)
+
+
+def no_repeats_if_last_n_same(sides, n, zoom):
+    """
+        the currently chosen vertex cannot be the same as the previously chosen vertex
+        if the two previously chosen vertices are the same.
+    """
+    verts = get_reg_poly_vertices(sides)
+    vt_q = [verts[0], ] * n
+    pt = verts[0]
+    draw_at(pt)
+    
+    for i in range(1, points + 1):
+        j = int(random() * sides)
+        
+        if vt_q.count(vt_q[0]) == len(vt_q) and vt_q[n - 1] == verts[j]:
+            i -= 1
+            continue
+        
+        vt_q.append(verts[j])
+        vt_q = vt_q[1:]
+        pt = (pt[0] + (vt_q[n - 1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[n - 1][1] - pt[1]) / zoom)
+        draw_at(pt)
+    
+    
+def no_neighbors_if_last_n_same(sides, n, zoom):
+    """
+        the currently chosen vertex cannot be one place away from the previously chosen vertex
+        if the two previously chosen vertices are the same.
+    """
+    verts = get_reg_poly_vertices(sides)
+    vt_q = [verts[0], ] * n
+    pt = verts[0]
+    draw_at(pt)
+    
+    for i in range(1, points + 1):
+        j = int(random() * sides)
+        
+        if vt_q.count(vt_q[0]) == len(vt_q) and \
+           (vt_q[n - 1] == verts[(j + 1) % sides] or vt_q[n - 1] == verts[(j - 1) % sides]):
+            i -= 1
+            continue
+        
+        vt_q.append(verts[j])
+        vt_q = vt_q[1:]
+        pt = (pt[0] + (vt_q[n - 1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[n - 1][1] - pt[1]) / zoom)
+        draw_at(pt)
+
+
+def no_cw_neighbors_if_last_n_same(sides, n, zoom):
+    """
+        the currently chosen vertex cannot be one place away (clockwise) from the previously chosen vertex
+        if the two previously chosen vertices are the same.
+    """
+    verts = get_reg_poly_vertices(sides)
+    vt_q = [verts[0], ] * n
+    pt = verts[0]
+    draw_at(pt)
+    
+    for i in range(1, points + 1):
+        j = int(random() * sides)
+        
+        if vt_q.count(vt_q[0]) == len(vt_q) and vt_q[n - 1] == verts[(j + 1) % sides]:
+            i -= 1
+            continue
+        
+        vt_q.append(verts[j])
+        vt_q = vt_q[1:]
+        pt = (pt[0] + (vt_q[n - 1][0] - pt[0]) / zoom,
+              pt[1] + (vt_q[n - 1][1] - pt[1]) / zoom)
         draw_at(pt)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from math import cos, pi, sin
+from math import cos, pi, sin, sqrt
 
 import pygame
 from pygame import display
@@ -12,24 +12,56 @@ bd = 25  # border
 w = h = size + 2 * bd
 window = pygame.display.set_mode((w, h))
 
-br = 128
+br = 255
 bg_clr = (0,) * 3
 pt_clr = (br,) * 3
 clrs = ((br, 0, 0), (0, br, 0), (0, 0, br),
         (br, br, 0), (br, 0, br), (0, br, br))
 
-iterations = 1 * pow(10, 6)
+max_br = 0  # to be updated as the fractal is drawn
+
+points = 2 * pow(10, 6)
 updates = 10
 
 
 def main():
     initial = float(time.time() * 1000)
     
+    # cool fractals for demonstration
+    
+    # fractal_avoiding_center_circle(100, 1.9)
     # fractal_avoiding_center_circle(100, 2)
+    # fractal_avoiding_center_circle(100, 2.5)
+    # restricted_fractal_2(3, 2)
+    # restricted_fractal_2(4, 2)
+    # no_repeats(5, 1.69)
+    # no_repeats(5, 2.59)
+    # no_repeats(6, 1.7)
+    # no_neighbors_n_away_cw(3, 1, 2.667)
+    # no_neighbors_n_away_cw(4, 2, 2)
+    # no_neighbors_n_away_cw(5, 1, 2)
+    # no_neighbors_n_away_cw(5, 2, 2)
     # no_cw_neighbors_2(4, 2.75)
+    # no_repeats_if_last_n_same(4, 2, 2)
+    # no_repeats_if_last_n_same(6, 4, 2)
+    # no_neighbors_if_last_n_same(4, 2, 2)
+    # no_neighbors_if_last_n_same(5, 2, 2)
+    no_neighbors_if_last_n_same(6, 2, 2)
+    
+    # no_cw_neighbors_if_last_n_same(4, 2, 2)
+    # no_cw_neighbors_if_last_n_same(5, 1, 2)
+    # no_cw_neighbors_if_last_n_same(5, 10, 2)
+    # no_cw_neighbors_if_last_n_same(6, 1, 1.475)
+    # no_cw_neighbors_if_last_n_same(6, 1, 1.8)
+    # no_cw_neighbors_if_last_n_same(6, 1, 1.9)
+    # no_cw_neighbors_if_last_n_same(6, 1, 2)
+    # no_cw_neighbors_if_last_n_same(6, 10, 1.85)
+    # no_cw_neighbors_if_last_n_same(8, 10, 1.7)
+    # no_cw_neighbors_if_last_n_same(10, 1, 1.8)
     
     final = float(time.time() * 1000)
     print(str(round((final - initial) / 1000, 3)) + 's')
+    # normalize_brightness()  # doesn't work
     
     while True:
         for event in pygame.event.get():
@@ -42,12 +74,16 @@ def main():
 
 def get_reg_poly_vertices(n):
     vertices = []
-    if n == 4:
+    if n == 3:
+        vertices = ((bd + size / 2, bd),
+                    (bd, bd + size * sqrt(3) / 2),
+                    (bd + size, bd + size * sqrt(3) / 2))
+    elif n == 4:
         vertices = ((bd, bd), (bd + size, bd), (bd + size, bd + size), (bd, bd + size))
     else:
         for i in range(n):
-            vertices.append((int(w / 2 + (size / 2) * cos(2 * pi * i / n - pi / 2)),
-                             int(h / 2 + (size / 2) * sin(2 * pi * i / n - pi / 2))))
+            vertices.append((w / 2 + (size / 2) * cos(2 * pi * i / n - pi / 2),
+                             h / 2 + (size / 2) * sin(2 * pi * i / n - pi / 2)))
     return vertices
 
 
@@ -58,17 +94,36 @@ def reg_poly_vertices(n):
 
 def draw_at(pt):
     """
-        If there is already a point at (x, y),
-        it's overwritten with more brightness.
+        If there is already a point at (x, y), its brightness is increased.
     """
-    x = pt[0]
-    y = pt[1]
-    cur_clr = window.get_at((x, y))
+    global max_br
+    x = int(pt[0])
+    y = int(pt[1])
+    pt_br = window.get_at((x, y))[0]
     
-    if cur_clr[0] < 224:
-        window.set_at((x, y), (cur_clr[0] + 32,) * 3)
-    elif cur_clr[0] == 224:
+    num_shades = 16  # the precision of the brightness
+    inc = 256 / num_shades
+    
+    if pt_br < 256 - inc:
+        window.set_at((x, y), (pt_br + inc,) * 3)
+        if pt_br + inc > max_br:
+            max_br = pt_br + inc
+    elif pt_br == 256 - inc:
         window.set_at((x, y), (255,) * 3)
+        max_br = 255
+
+
+def normalize_brightness():
+    """
+        Normalizes the brightness of the fractal to the range [0, 255].
+        Best to use 256 shades when using this function
+    """
+    for x in range(bd, w):
+        for y in range(bd, h):
+            cur_clr = window.get_at((x, y))
+            if cur_clr[0] > 0:
+                print(max_br)
+                window.set_at((x, y), (int(cur_clr[0] * 255 / max_br),) * 3)
 
 
 if __name__ == '__main__':
